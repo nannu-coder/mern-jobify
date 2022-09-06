4210a3037b710c255e960ab43946825728f0afe2
nannu-coder <nazmulnannu43@gmail.com>
2022-09-06 22:36:33 +0600

    added all file

---
+++ server.js
@@ -0,0 +1,55 @@
+import express from "express";
+const app = express();
+import dotenv from "dotenv";
+dotenv.config();
+import "express-async-errors";
+import morgan from "morgan";
+import cors from "cors";
+
+import mongoSanitize from "express-mongo-sanitize";
+
+// hello
+// db and authenticateUser
+import connectDB from "./db/connect.js";
+
+// routers
+import authRouter from "./routes/authRoutes.js";
+import jobsRouter from "./routes/jobsRoutes.js";
+
+// middleware
+import notFoundMiddleware from "./middleware/not-found.js";
+import errorHandlerMiddleware from "./middleware/error-handler.js";
+import authenticateUser from "./middleware/auth.js";
+
+if (process.env.NODE_ENV !== "production") {
+  app.use(morgan("dev"));
+}
+
+app.use(express.json());
+app.use(mongoSanitize());
+app.use(cors());
+
+app.use("/api/v1/auth", authRouter);
+app.use("/api/v1/jobs", authenticateUser, jobsRouter);
+
+app.use(notFoundMiddleware);
+app.use(errorHandlerMiddleware);
+
+// if (process.env.NODE_ENV === "production") {
+//   app.use(express.static("client/build"));
+// }
+
+const port = process.env.PORT || 5000;
+
+const start = async () => {
+  try {
+    await connectDB(process.env.MONGO_URL);
+    app.listen(port, () => {
+      console.log(`Server is listening on port ${port}...`);
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+start();
